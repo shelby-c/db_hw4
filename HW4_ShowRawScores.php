9c9efb596e521cbc56d1c9a8330b62675f331a1c
shelby <coe.shelby@gmail.com>
2022-04-18 16:54:14 -0400

    debugging

--- HW4_ShowRawScores.php
+++ HW4_ShowRawScores.php
@@ -1,50 +1,6 @@
  <head><title>HW4 Show Raw Scores</title></head>
  <body>
 <?php 
-    echo "<h2>HW4 Show Raw Scores</h2>";
-    echo "<br>";
- 
-    // open a connection to dbase server
-    include 'open.php'; 
- 
-    // collect the posted value in a variable called $item
-    $password = $_POST['SID'];;
- 
-    // prepare query statement
-    echo "The password collected from the form was ";
-    if(!empty($password)) {
-        echo $password;
-        echo "<br><br>";
-        // execute it, and if non-empty result, output each row of result
-        if ($result = $conn->query("CALL HW4_ShowRawScores('".$password."');")) {
-  
-            echo "<table border=\"2px solid black\">";
-            echo "<tr><td>SID</td><td>LName</td><td>FName</td><td>Sec</td><td>AName</td><td>Score</td></tr>";
-                foreach($result as $row){
-                   echo "<tr>";
-                   echo "<td>".$row["SID"]."</td>";
-                   echo "<td>".$row["LName"]."</td>";
-                   echo "<td>".$row["FName"]."</td>";
-                   echo "<td>".$row["Sec"]."</td>";
-                   echo "<td>".$row["AName"]."</td>";
-                   echo "<td>".$row["Score"]."</td>";
-                   echo "</tr>";
-                }
-            echo "</table>";
-        } else {
-            echo "Call to AllRawScores failed<br>";
-        }
-    } else {
-         echo "not set";
-    }
-    echo ".<br>";
- 
-     
-    $conn->close();
-
-
-
-/*
     echo "<h2>HW4 Show Raw Score</h2>";
     echo "<br>";
 
@@ -91,6 +47,6 @@
 
     
     $conn->close();
-*/
+
 ?>
 </body>
