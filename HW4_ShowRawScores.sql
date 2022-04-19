3a456ab3cb7e427c1f970a9d8e5bfde96a49e37f
shelby <coe.shelby@gmail.com>
2022-04-19 13:03:27 -0400

    debugging

--- HW4_ShowRawScores.sql
+++ HW4_ShowRawScores.sql
@@ -70,11 +70,9 @@ BEGIN
    -- into a larger query string so we can execute it, but leave ?
    -- in place so we can plug in the specific sid value in a careful way
    
-   SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, HW4_RawScore.Score AS Score, HW4_RawScore.AName AS AName
-                             FROM HW4_Student, HW4_RawScore
-                             WHERE HW4_Student.SID = HW4_RawScore.SID ', 'SELECT SID, LName, FName, Sec, ',
+   SET @sql = CONCAT('SELECT sid, ',
                      @sql,
-                     ' FROM StudentScores WHERE sid = ',
+                     ' FROM HW4_RawScore WHERE sid = ',
 		     '?');
 
    -- alert the server we have a statement shell to set up
