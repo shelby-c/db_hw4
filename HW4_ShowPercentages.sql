6413e4e0814a876c28a05cc35c3a82ee988b661a
shelby <coe.shelby@gmail.com>
2022-04-19 21:47:37 -0400

    debugging

--- HW4_ShowPercentages.sql
+++ HW4_ShowPercentages.sql
@@ -21,7 +21,7 @@ DELIMITER //
 
 -- Get grades as a percentage for each assignment
 -- NEED TO ACCOUNT FOR UNATTEMPTED ASSIGNMENTS
-DROP VIEW IF EXISTS AssignmentPercentages;
+/*DROP VIEW IF EXISTS AssignmentPercentages;
 
 CREATE VIEW AssignmentPercentages AS
 SELECT HW4_RawScore.SID AS SID, HW4_Assignment.AName AS AName, (HW4_RawScore.Score / HW4_Assignment.PtsPoss) AS AssignmentPercent, HW4_Assignment.AType AS AType
@@ -56,6 +56,87 @@ BEGIN
    ELSE
        SELECT CONCAT('ERROR: SID ', sid, ' not found') AS SID;
    END IF;
+END; //
+*/
+
+DROP PROCEDURE IF EXISTS HW4_ShowRawScores //
+
+CREATE PROCEDURE HW4_ShowRawScores(IN sid VARCHAR(4))
+BEGIN
+   -- IF EXISTS(SELECT * FROM HW4_Student WHERE HW4_Student.SID = sid) THEN
+--   IF CalcBidCount(item) > 0 THEN -- need it to read like "if exists"
+      /*WITH EveryAssignment AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, HW4_Assignment.AName AS AName
+                                FROM HW4_Student CROSS JOIN HW4_Assignment) -- all students matched with all assignments
+      SELECT EveryAssignment.SID, EveryAssignment.LName, EveryAssignment.FName, EveryAssignment.Sec, EveryAssignment.AName, HW4_RawScore.Score
+      FROM EveryAssignment LEFT OUTER JOIN HW4_RawScore
+        ON HW4_RawScore.AName = EveryAssignment.AName AND EveryAssignment.SID = HW4_RawScore.SID
+      WHERE HW4_RawScore.SID = sid;*/
+
+
+      -- try 2
+     /* WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, HW4_RawScore.Score AS Score, HW4_RawScore.AName AS AName
+                             FROM HW4_Student, HW4_RawScore
+                             WHERE HW4_Student.SID = HW4_RawScore.SID AND HW4_Student.SID = sid) -- all students matched with scores for assignments they attempted
+      SELECT StudentScores.SID, StudentScores.LName, StudentScores.FName, StudentScores.Sec, HW4_Assignment.AName, StudentScores.Score
+      FROM StudentScores RIGHT OUTER JOIN HW4_Assignment
+        ON StudentScores.AName = HW4_Assignment.AName;*/
+      -- WHERE StudentScores.SID = sid;
+
+      /*
+        SELECT WithSID.SID, WithSID.LName, WithSID.FName, WithSID.Sec, HW4_Assignment.AName, WithSID.Score
+      FROM (SELECT * 
+            FROM StudentScores
+            WHERE StudentScores.SID = sid) AS WithSID RIGHT OUTER JOIN HW4_Assignment
+        ON WithSID.AName = HW4_Assignment.AName;
+      */
+   /*ELSE
+      SELECT CONCAT('ERROR: SID ', sid, ' not found') AS SID;
+   END IF;*/
+    DROP PROCEDURE IF EXISTS HW4_ShowPercentages //
+
+    CREATE PROCEDURE HW4_ShowPercentages(IN sid VARCHAR(4))
+    BEGIN
+
+    IF EXISTS(SELECT * FROM HW4_Student WHERE HW4_Student.SID = sid) THEN
+        --  MODIFIED CODE FROM 4/19 CLASS
+        SET @sql = NULL;
+
+        -- accumulate into the variable named @sql a list of assignment names
+        -- and expressions to that will get the associated scores, for use 
+        -- as part of a later query of table HW4_RawScore
+        SELECT
+            GROUP_CONCAT(DISTINCT
+            CONCAT(
+                'max(case when aname = ''',
+                aname,
+                ''' then score end) as ''',aname,''''
+            )
+            ORDER BY atype DESC, aname ASC
+            ) INTO @sql
+        FROM HW4_Assignment;
+
+        -- concatenate the assignment name list and associated expressions
+        -- into a larger query string so we can execute it, but leave ?
+        -- in place so we can plug in the specific sid value in a careful way
+   
+        SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, AssignmentPercentages.AssignmentPercent AS AssignmentPercent, AssignmentPercentages.AName AS AName
+                             FROM HW4_Student, AssignmentPercentages
+                             WHERE HW4_Student.SID = AssignmentPercentages.SID) ', 'SELECT sid, LName, FName, Sec, ',
+                     @sql,
+                     ' FROM StudentScores WHERE sid = ',
+		     '?');
+
+        -- alert the server we have a statement shell to set up
+        PREPARE stmt FROM @sql;
+
+         -- now execute the statement shell with a value plugged in for the ?
+        EXECUTE stmt USING sid;
+
+        -- tear down the prepared shell since no longer needed (we won't requery it)
+        DEALLOCATE PREPARE stmt;
+    ELSE
+      SELECT CONCAT('ERROR: SID ', sid, ' not found') AS SID;
+    END IF;
 END; //
 
 DELIMITER ;
