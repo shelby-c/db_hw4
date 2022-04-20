ac70c88bf91e6dba022f2c6574282c9fec7bcdf2
shelby <coe.shelby@gmail.com>
2022-04-19 20:35:45 -0400

    debugging

--- HW4_ShowAllRawScores.sql
+++ HW4_ShowAllRawScores.sql
@@ -24,42 +24,45 @@ BEGIN
       SELECT '(all raw scores) ERROR: Invalid password' AS SID;  -- DELETE () BEFORE SUBMITTING
    END IF;*/
 
-    --  MODIFIED CODE FROM 4/19 CLASS
-    SET @sql = NULL;
+    IF EXISTS(SELECT * FROM HW4_Password WHERE HW4_Password.CurPasswords = Password) THEN
+        --  MODIFIED CODE FROM 4/19 CLASS
+        SET @sql = NULL;
 
-   -- accumulate into the variable named @sql a list of assignment names
-   -- and expressions to that will get the associated scores, for use 
-   -- as part of a later query of table HW4_RawScore
-   SELECT
-     GROUP_CONCAT(DISTINCT
-       CONCAT(
-         'max(case when aname = ''',
-         aname,
-         ''' then score end) as ''',aname,''''
-       )
-       ORDER BY atype DESC, aname ASC
-     ) INTO @sql
-   FROM HW4_Assignment;
+        -- accumulate into the variable named @sql a list of assignment names
+        -- and expressions to that will get the associated scores, for use 
+        -- as part of a later query of table HW4_RawScore
+        SELECT
+            GROUP_CONCAT(DISTINCT
+            CONCAT(
+                'max(case when aname = ''',
+                aname,
+                ''' then score end) as ''',aname,''''
+            )
+            ORDER BY atype DESC, aname ASC
+            ) INTO @sql
+        FROM HW4_Assignment;
 
-   -- concatenate the assignment name list and associated expressions
-   -- into a larger query string so we can execute it, but leave ?
-   -- in place so we can plug in the specific sid value in a careful way
+        -- concatenate the assignment name list and associated expressions
+        -- into a larger query string so we can execute it, but leave ?
+        -- in place so we can plug in the specific sid value in a careful way
    
-   SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, HW4_RawScore.Score AS Score, HW4_RawScore.AName AS AName
+        SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, HW4_RawScore.Score AS Score, HW4_RawScore.AName AS AName
                              FROM HW4_Student, HW4_RawScore
                              WHERE HW4_Student.SID = HW4_RawScore.SID) ', 'SELECT sid, LName, FName, Sec, ',
                      @sql,
-                     ' FROM StudentScores WHERE sid = ',
-		     '?');
+                     ' FROM StudentScores ', 'ORDER BY StudentScores.Sec ASC, StudentScores.LName ASC, StudentScores.FName ASC');
 
-   -- alert the server we have a statement shell to set up
-   PREPARE stmt FROM @sql;
+        -- alert the server we have a statement shell to set up
+        PREPARE stmt FROM @sql;
 
-   -- now execute the statement shell with a value plugged in for the ?
-   EXECUTE stmt USING sid;
+         -- now execute the statement shell with a value plugged in for the ?
+        EXECUTE stmt USING sid;
 
-   -- tear down the prepared shell since no longer needed (we won't requery it)
-   DEALLOCATE PREPARE stmt;
+        -- tear down the prepared shell since no longer needed (we won't requery it)
+        DEALLOCATE PREPARE stmt;
+    ELSE
+      SELECT CONCAT('ERROR: SID ', sid, ' not found') AS SID;
+    END IF;
 END; //
 
 DELIMITER ;
