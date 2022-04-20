5d344e69df58c52df6b106caa6ab19ad2e6da2fc
shelby <coe.shelby@gmail.com>
2022-04-19 22:41:52 -0400

    debugging

--- HW4_ShowPercentages.sql
+++ HW4_ShowPercentages.sql
@@ -55,21 +55,21 @@ DELIMITER //
         -- into a larger query string so we can execute it, but leave ?
         -- in place so we can plug in the specific sid value in a careful way
    
-        -- why doesn't this work? should be same as one below which works fir ShowRawScores but with AssignmentPercentages instead of HW4_RawScore
-        /*SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, AssignmentPercentages.Score AS Score, AssignmentPercentages.AName AS AName
-                             FROM HW4_Student, AssignmentPercentages
-                             WHERE HW4_Student.SID = AssignmentPercentages.SID) ', 'SELECT sid, LName, FName, Sec, ',
-                     @sql,
-                     ' FROM StudentScores WHERE sid = ',
-		     '?');*/
-
-        
+        -- why doesn't this work? should be same as one below which works for ShowRawScores but with AssignmentPercentages instead of HW4_RawScore
         SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, HW4_RawScore.Score AS Score, HW4_RawScore.AName AS AName
                              FROM HW4_Student, HW4_RawScore
                              WHERE HW4_Student.SID = HW4_RawScore.SID) ', 'SELECT sid, LName, FName, Sec, ',
                      @sql,
                      ' FROM StudentScores WHERE sid = ',
 		     '?');
+
+        
+        /*SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, HW4_RawScore.Score AS Score, HW4_RawScore.AName AS AName
+                             FROM HW4_Student, HW4_RawScore
+                             WHERE HW4_Student.SID = HW4_RawScore.SID) ', 'SELECT sid, LName, FName, Sec, ',
+                     @sql,
+                     ' FROM StudentScores WHERE sid = ',
+		     '?');*/
         
 
         -- alert the server we have a statement shell to set up
