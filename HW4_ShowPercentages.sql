a64983e6231f06076300d6e3b86821d814a81b48
shelby <coe.shelby@gmail.com>
2022-04-20 21:20:03 -0400

    trying

--- HW4_ShowPercentages.sql
+++ HW4_ShowPercentages.sql
@@ -99,9 +99,9 @@ DELIMITER //
                      ' FROM StudentScores WHERE sid = ',
 		     '?');*/
 
-        SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, HW4_RawScore.Score AS Score, HW4_RawScore.AName AS AName
-                             FROM HW4_Student, HW4_RawScore
-                             WHERE HW4_Student.SID = HW4_RawScore.SID) ', 'SELECT sid, LName, FName, Sec, ',
+        SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, Assignments.Score AS Score, Assignments.AName AS AName
+                             FROM HW4_Student, Assignments
+                             WHERE HW4_Student.SID = Assignments.SID) ', 'SELECT sid, LName, FName, Sec, ',
                      @sql,
                      ' FROM StudentScores WHERE sid = ',
 		     '?');
