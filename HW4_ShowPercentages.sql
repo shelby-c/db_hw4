722499b356f07279195951bbf652991d53fbcea1
shelby <coe.shelby@gmail.com>
2022-04-20 20:48:34 -0400

    debugging

--- HW4_ShowPercentages.sql
+++ HW4_ShowPercentages.sql
@@ -28,7 +28,7 @@ ON HW4_RawScore.AName = HW4_Assignment.AName;
 DROP VIEW IF EXISTS CourseAverages; -- course avg for 1006 should be 78.3999
 
 CREATE VIEW CourseAverages AS
-SELECT ExamPercentages.SID AS SID, FORMAT(COALESCE(QuizAvg, 0) * 0.4 + COALESCE(ExamAvg, 0) * 0.6, 2) AS CourseAvg
+SELECT ExamPercentages.SID AS SID, COALESCE(QuizAvg, 0) * 0.4 + COALESCE(ExamAvg, 0) * 0.6 AS CourseAvg
 FROM (SELECT AVG(IFNULL(Assignments.Score, 0)) AS ExamAvg, Assignments.SID AS SID
       FROM Assignments
       WHERE Assignments.AType = 'EXAM'
@@ -85,12 +85,12 @@ DELIMITER //
         -- in place so we can plug in the specific sid value in a careful way
    
         -- why doesn't this work? should be same as one below which works for ShowRawScores but with AssignmentPercentages instead of HW4_RawScore
-      /*SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, Assignments.Score AS Score, Assignments.AName AS AName
+      SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, Assignments.Score AS Score, Assignments.AName AS AName
                              FROM HW4_Student, Assignments
                              WHERE HW4_Student.SID = Assignments.SID) ', 'SELECT StudentScores.sid, StudentScores.LName, StudentScores.FName, StudentScores.Sec, CourseAverages.CourseAvg, ', 
                      @sql, 
                      ' FROM StudentScores, CourseAverages WHERE StudentScores.SID = CourseAverages.SID AND StudentScores.SID = ',
-		     '?');*/
+		     '?');
 
         /*SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, Assignments.Score AS Score, Assignments.AName AS AName
                              FROM HW4_Student, Assignments
@@ -98,13 +98,6 @@ DELIMITER //
                      @sql, 
                      ' FROM StudentScores WHERE sid = ',
 		     '?');*/
-        
-        SET @sql = CONCAT('WITH StudentScores AS (SELECT HW4_Student.SID AS SID, HW4_Student.LName AS LName, HW4_Student.FName AS FName, HW4_Student.Sec AS Sec, Assignments.Score AS Score, Assignments.AName AS AName
-                             FROM HW4_Student, Assignments
-                             WHERE HW4_Student.SID = Assignments.SID) ', 'SELECT StudentScores.sid, StudentScores.LName, StudentScores.FName, StudentScores.Sec, CourseAverages.CourseAvg, ', 
-                     @sql, 
-                     ' FROM StudentScores, CourseAverages WHERE StudentScores.SID = CourseAverages.SID AND StudentScores.SID = ',
-		     '?');
 
 
        
